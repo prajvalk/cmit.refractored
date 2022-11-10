296df43fd3bf1324d42a4dc72fcde3e7a132f55c
Chlorine Pentoxide <prajvalk.digital@gmail.com>
2022-11-10 21:01:17 +0530

    More (stable) refractoring ...

--- refractor.sh
+++ refractor.sh
@@ -4,5 +4,5 @@ set -e
 
 for file in $(find -follow | grep .html) ; do
     echo "Patching $file ... "
-    sed -i -e 's@https://cmit.iisertvm.ac.in/style.main.min.9b6ec9095a93a45c60e57b6fb77b9d7027eaba5e549fb71506aa3e312b55d55d.css@../style.main.min.9b6ec9095a93a45c60e57b6fb77b9d7027eaba5e549fb71506aa3e312b55d55d.css@g' $file
+    sed -i -e 's@../style.main.min.9b6ec9095a93a45c60e57b6fb77b9d7027eaba5e549fb71506aa3e312b55d55d.css@/style.main.min.9b6ec9095a93a45c60e57b6fb77b9d7027eaba5e549fb71506aa3e312b55d55d.css@g' $file
 done
